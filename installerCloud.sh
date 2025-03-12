a2cd4859faf8529d083d53f6aff67e5470c7c309
basfuentesp <bas.fuentesp@profesor.duoc.cl>
2025-03-12 17:29:14 -0300

    add config build

--- installerCloud.sh
+++ installerCloud.sh
@@ -1,2 +1,14 @@
+#Genenar jar del proyecto en carpeta target
 mvn clean install
 
+#Cambiar permisos a jar para ejecucion
+chmod +x target/challengeApp.jar 
+
+#Copiar archivo a directorio actual de build
+cp target/challengeApp.jar .
+
+#Construir y subir imagen Docker a GCP
+gcloud builds submit . --tag us.grc.io/challenge/bf_challenge_api:latest
+
+#Eliminar jar utilizado
+rm challengeApp.jar
