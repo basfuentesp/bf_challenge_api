588d4638934eb214c71c45056134c16d767924f9
basfuentesp <bas.fuentesp@profesor.duoc.cl>
2025-03-12 03:02:49 -0300

    new service insert

---
+++ src/main/java/bf/challenge/service/ServicioInsert.java
@@ -0,0 +1,14 @@
+package bf.challenge.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioInsert {
+	private static Logger logs = LoggerFactory.getLogger(ServicioInicio.class);
+	
+	public saveInsert() {
+		
+	}
+}
--- src/main/java/bf/challenge/service/ServicioJDBC.java
+++ src/main/java/bf/challenge/service/ServicioJDBC.java
@@ -3,7 +3,11 @@ package bf.challenge.service;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +21,23 @@ import com.opencsv.exceptions.CsvValidationException;
 @Service
 public class ServicioJDBC {
 	private static Logger logs = LoggerFactory.getLogger(ServicioInicio.class);
+	
+	private Statement stm = null;
+	private ResultSet rs = null;
+	private boolean outputs = false;
+	
+	public ServicioJDBC(){
 		
+	}
+	
+	public String doURLCon(String host, String port, String database) {
+		return "jdbc:mysql://"+host+":"+port+"/"+database;
+	}
+	
+	private Connection doConnect(String host, String port, String user, String password) throws SQLException {
+		return DriverManager.getConnection(doURLCon(host, port, password), user, password);
+	}
+	
 	public boolean doQuery() throws IOException, SQLException {
 		String host = "localhost";
 		String port = "3306";
@@ -25,21 +45,34 @@ public class ServicioJDBC {
 		String password = "null";
 		
 		String table = "challenge.Jobs";
-		String query = "insert into";
+		String query = "insert into ";
 		String fetchsize = "100000";
 		
 		logs.info("Se inicia sesion con BD MySQL");
+		Connection con = doConnect(host,port,user,password);
 		
-		Connection connection = doConnect(host,port,user,password);
-		
-		//Tablas
-		
-		return false;
+		try {
+			con.setAutoCommit(false);
+			
+			stm = con.createStatement();
+			
+			stm.setFetchSize(Integer.parseInt(fetchsize));
+			
+			rs = stm.executeQuery(query);
+			
+			outputs = inserts.saveInsert(rs, table, ".csv");
+			
+		} catch (SQLException e) {
+			logs.info(e.getMessage());
+		} finally {
+			if (rs != null) {
+				rs.close();
+				stm.close();
+			}
+		}
+		return outputs;
 	}
 	
 	
-	private Connection doConnect(String host, String port, String user, String password) {
-		return null;
-	}
 	
 }
